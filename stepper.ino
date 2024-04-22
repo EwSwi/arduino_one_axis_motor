8787586a781e979f29e46db12b93cc0262eadb80
EwSwi <143598808+EwSwi@users.noreply.github.com>
2024-04-22 10:38:48 +0200

    Update stepper.ino
    adding chimney option

--- stepper.ino
+++ stepper.ino
@@ -6,26 +6,50 @@ AccelStepper myStepper(AccelStepper::DRIVER, stepPin, dirPin);
 
 int stepsPerMM = 200;
 float Z0 = 2180;
-float targetZ = 2170;
+float targetZ = 2181;
 float current_Z;
 
 float limitUP = 2390;
 float limitDOWN = 2150;
-bool clockwise = true;
+bool chimneyDetected = false; // Initial state of chimney (assume no chimney)
 
 void moveMotor(float targetPosition) {
   myStepper.moveTo(targetPosition);
 }
 
+bool checkForChimney() {
+  bool chimney;
+  Serial.print("Is there an evaporating chimney? (y/n): ");
+  
+  while (Serial.available() == 0);
+  
+  char inputChar = Serial.read();
+  
+  if (inputChar == 'y' || inputChar == 'Y') {
+    return true; 
+  } else if (inputChar == 'n' || inputChar == 'N') {
+    return false; 
+  } else {
+    return false; 
+  }
+}
+
 void setup() {
+  myStepper.setAcceleration(2000); 
+  myStepper.setMaxSpeed(100);
   Serial.begin(9600);
-  myStepper.setMaxSpeed(100); 
-  myStepper.setAcceleration(500);
 }
 
 void loop() {
   myStepper.run();
   current_Z = Z0 + (float)myStepper.currentPosition() / stepsPerMM;
+  if (!chimneyDetected) {
+    chimneyDetected = true;
+  if (checkForChimney()) {
+  myStepper.setMaxSpeed(50);
+   myStepper.moveTo(2180); 
+ }
+
   if (targetZ >= limitDOWN && targetZ <= limitUP) {
     float targetPosition = (targetZ - Z0) * stepsPerMM;
     moveMotor(targetPosition);
@@ -33,6 +57,6 @@ void loop() {
     myStepper.stop();
     Serial.println("Target position is out of range.");
   }
-  Serial.print("Current Z position: ");
-  Serial.println(current_Z);
+  }
+  
 }
